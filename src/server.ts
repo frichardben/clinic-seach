5212ba1c14ad596306a80a8e8a5fa984e83ee6e2
Richard Ben <frichardben@yahoo.com.br>
2023-03-19 23:52:59 -0300

    chore: added express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import { routes } from './routes';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internnal server error',
+    });
+  }
+);
 
 app.listen(3333);
 
